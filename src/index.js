b4923eb65caa83cede6eb892950f16b677895f7d
arikarim <arikarim146@gmail.com>
2021-04-25 22:02:09 +0300

    Append elements to the dom

--- src/index.js
+++ src/index.js
@@ -4,11 +4,21 @@ const submitButton = document.getElementById('project-submit-button');
 const projectList = document.getElementById('project-list');
 const form = document.getElementById('project-form')
 
+
 submitButton.addEventListener('click', (e) => {
   e.preventDefault();
   const formInput = document.getElementById('project-input').value;
   const projectDiv = document.createElement('div');
-  projectDiv.textContent = formInput;
+  const divChild1 = document.createElement('h4');
+  const divChild2 = document.createElement('div');
+
+  projectDiv.appendChild(divChild1)
+  projectDiv.appendChild(divChild2)
+
+  projectDiv.classList.add('d-flex');
+  projectDiv.setAttribute('data-tab-content', '')
+
+  divChild1.textContent = formInput;
   projectList.appendChild(projectDiv);
   form.reset();
 })
