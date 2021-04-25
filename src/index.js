cd29136ed39aa1a434867af1078a4524edb21c51
arikarim <arikarim146@gmail.com>
2021-04-25 21:38:13 +0300

    Add form to the DOM

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import './style.css';
 
-const formInput = document.getElementById('project-input');
-const submitButton = document.getElementById('project-submit-button')
+const submitButton = document.getElementById('project-submit-button');
+const projectList = document.getElementById('project-list');
+const form = document.getElementById('project-form')
+
+submitButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  const formInput = document.getElementById('project-input').value;
+  const projectDiv = document.createElement('div');
+  projectDiv.textContent = formInput;
+  projectList.appendChild(projectDiv);
+  form.reset();
+})
