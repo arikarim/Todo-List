573513f081eed6bf62c22e1016225517505c4a0d
arikarim <arikarim146@gmail.com>
2021-04-26 00:31:31 +0300

    Add Projects to DOM

--- src/project.js
+++ src/project.js
@@ -3,15 +3,39 @@ const createProject = () => {
 
 
   const listsContainer = document.querySelector('[data-lists]');
+  const newListForm = document.querySelector('[data-new-list-form]')
+  const newListinput = document.querySelector('[data-new-list-input]')
 
-  let lists = [];
+  let lists = [{
+    id: 1,
+    name: "ari"
+  },
+  {
+    id: 2,
+    name: 'Nigar'
+  }];
+
+  newListForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const listName = newListinput.value;
+    if(listName == null || listName === '') return
+    const list = createList(listName)
+    newListinput.value = '';
+    lists.push(list)
+    render()
+  })
+
+  function createList(name) {
+    return { id: Date.now().toString(), name: name, tasks: []}
+  }
 
   function render() {
     clearElement(listsContainer)
     lists.forEach(list => {
       const listElement = document.createElement('li');
+      listElement.dataset.listId = list.id
       listElement.classList.add('list-name');
-      listElement.innerText = list;
+      listElement.innerText = list.name;
       listsContainer.appendChild(listElement)
     })
   }
